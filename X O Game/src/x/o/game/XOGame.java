e4e65e44b1b77e63b70d9ab91796f7ac8b94a32f
Naiim <naiimhassouna0@gmail.com>
2024-02-02 18:50:22 +0200

    first commit

---
+++ X O Game/src/x/o/game/XOGame.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package x.o.game;
+
+/**
+ *
+ * @author User
+ */
+public class XOGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+		TicTacToe tictactoe = new TicTacToe();
+		
+	}
+}
